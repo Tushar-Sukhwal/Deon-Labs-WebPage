83e996a69f63bdc4dba6ec8175b4269b5d659d77
Tushar Sukhwal <tushars7740@gmail.com>
2023-07-12 22:46:07 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,15 @@
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <ul className="flex ">
+        <li>Deon-Labs</li>
+        <li>Overview</li>
+        <li>Roadmap</li>
+        <li>Docs</li>
+        <li>Social</li>
+        <li>Launch App</li>
+      </ul>
+    </div>
   )
 }
 export default NavBar
