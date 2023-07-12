3824241c0f4bb742a6cb560f4fb59d8b2c41b622
Tushar Sukhwal <tushars7740@gmail.com>
2023-07-12 23:14:57 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
+
+
+
 const NavBar = () => {
   return (
-    <div>
-      <ul className="flex ">
-        <li>Deon-Labs</li>
-        <li>Overview</li>
-        <li>Roadmap</li>
-        <li>Docs</li>
-        <li>Social</li>
-        <li>Launch App</li>
+    <div className="w-[100vw] bg-[#0E0E0E] pb-3">
+      <ul className="flex w-[50%] justify-around m-auto items-center pt-3">
+        <li className="p-3 text-xl text-white text-center "><span className="text-3xl align-middle">Deon </span><br />Labs</li>
+        <li className="p-3 text-red-700">Overview</li>
+        <li className="p-3 text-white">Roadmap</li>
+        <li className="p-3 text-white">Docs</li>
+        <li className="p-3 text-white">Social</li>
+        <li className="bg-[#FF306E] opacity-80 text-white p-3 px-5  items-baseline align-middle  rounded-lg"><h1 className=" opacity-100"> Launch App</h1></li>
       </ul>
     </div>
   )
