1b0fb7b69724017f13ae4e78c244ad47a5cdfce5
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2023-07-15 04:00:46 +0530

    working commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,20 +15,22 @@ function App() {
             <Team />
             <Footer />
 
-            <AnimatedCursor
-                innerSize={8}
-                outerSize={35}
-                innerScale={1}
-                outerScale={2}
-                outerAlpha={0}
-                hasBlendMode={true}
-                innerStyle={{
-                    backgroundColor: "#fff",
-                }}
-                outerStyle={{
-                    border: "3px solid #fff",
-                }}
-            />
+            <div id="cursor">
+                <AnimatedCursor
+                    innerSize={8}
+                    outerSize={35}
+                    innerScale={1}
+                    outerScale={2}
+                    outerAlpha={0}
+                    hasBlendMode={true}
+                    innerStyle={{
+                        backgroundColor: "#fff",
+                    }}
+                    outerStyle={{
+                        border: "3px solid #fff",
+                    }}
+                />
+            </div>
         </div>
     );
 }
--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -34,19 +34,33 @@ const companies = [
 ];
 
 const Companies = () => {
+    const onMouseIn = (e) => {
+        const cursor = document.querySelector("#cursor").firstChild;
+        cursor.innerHTML = `<div class="cursor-text">${
+            e.target.src.split("/").pop().split(".")[0]
+        }</div>`;
+    };
+
+    const onMoueOut = () => {
+        const cursor = document.querySelector("#cursor").firstChild;
+        cursor.innerText = "";
+    };
+
     return (
-        <div className="flex flex-col justify-center items-center w-full bg-black pb-20">
+        <div className="flex flex-col justify-center items-center w-full bg-black pb-28">
             <h1 className="text-white text-5xl ">Partners</h1>
             <div className="marquee">
                 <ul className="marquee__content">
                     {companies.map((company, idx) => (
                         <div
                             key={`${company}#${idx}`}
-                            className="m-5 w-20 flex">
+                            className="m-5 w-28 flex">
                             <img
                                 src={company}
                                 alt=""
                                 className="object-contain"
+                                onMouseEnter={onMouseIn}
+                                onMouseLeave={onMoueOut}
                             />
                         </div>
                     ))}
@@ -55,11 +69,13 @@ const Companies = () => {
                     {companies.map((company, idx) => (
                         <div
                             key={`${company}#${idx}`}
-                            className="m-5 w-20 flex">
+                            className="m-5 w-28 flex">
                             <img
                                 src={company}
                                 alt=""
                                 className="object-contain"
+                                onMouseEnter={onMouseIn}
+                                onMouseLeave={onMoueOut}
                             />
                         </div>
                     ))}
@@ -68,11 +84,13 @@ const Companies = () => {
                     {companies.map((company, idx) => (
                         <div
                             key={`${company}#${idx}`}
-                            className="m-5 w-20 flex">
+                            className="m-5 w-28 flex">
                             <img
                                 src={company}
                                 alt=""
                                 className="object-contain"
+                                onMouseEnter={onMouseIn}
+                                onMouseLeave={onMoueOut}
                             />
                         </div>
                     ))}
