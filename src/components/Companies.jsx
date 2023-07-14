bc3e436cb933723ac871fba4fb564711fb1b8c7d
Tushar Sukhwal <tushars7740@gmail.com>
2023-07-14 15:17:47 +0530

    commit

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -4,8 +4,9 @@ const companies = [
 	"./flow.png",
 	"./amazon.png",
 	"./google.png",
-	"./filecoin.png",
-	"./shardeum.png",,
+	"./filcoin.png",
+	"./shardeum.png",
+	,
 	"./chainlink.png",
 	"./5ire.svg",
 	"./ipfs.png",
@@ -20,13 +21,11 @@ const companies = [
 const Companies = () => {
 	return (
 		<div className="flex flex-col justify-center items-center w-[100vw] bg-black pb-20">
-      <h1 className="text-white text-4xl ">
-        Partner compaines
-      </h1>
-			<div className="flex text-white pt-10 flex-wrap items-center justify-center">
+			<h1 className="text-white text-4xl ">Partner compaines</h1>
+			<div className="flex text-white pt-10 flex-wrap items-center justify-center  bg-gray-50/40 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]  m-10  rounded-xl">
 				{companies.map((company) => (
-					<div className="card m-4">
-						<div className="card-body w-20 h-10">
+					<div className="m-4">
+						<div className=" w-20 ">
 							<img src={company} alt="" />
 						</div>
 					</div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
 				Web3 Ecosystem
 				<br /> Enabler
 			</p>
-			<div className="flex flex-col items-center w-976 h-103 flex-shrink-0 pt-20px pb-20px">
+			<div className="flex flex-col items-center flex-shrink-0 pt-20px pb-20px">
 				<p className="text-white text-center font-inter text-xl font-normal ">
 					Deon Labs is your In-house Web3 Ecosystem builder driving paradigm{" "}
 					<br /> shift into Web3 Ecosystems.
